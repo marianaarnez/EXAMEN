cabcc022344573c10591f831319669937de80558
mariana <visual_community@outlook.com>
2019-09-28 09:44:20 -0400

    web api OK

---
+++ Arnez/Arnez/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Arnez.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Arnez.Models.Fairy> Fairies { get; set; }
+    }
+}
---
+++ Arnez/Arnez/Models/Fairy.cs
@@ -0,0 +1,36 @@
+﻿namespace Arnez.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Fairy
+
+    {
+        public enum WishType
+
+        {
+            Avion,
+            Abeja,
+            Avispa
+
+        }
+        [Key]
+        public int FairyID { get; set; }
+
+        [Required]
+        [Display(Name ="Nombre Completo")]
+        [StringLength(24,MinimumLength =2)]
+        public string NickName { get; set; }
+
+        [Required]
+        public WishType Wishes { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Direccion de correo no valida")]
+        public string Email { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+
+    }
+}
